3cb9c2ff0880bf42ea0c289f5780fed0bf16d608
ksshlee <ksshlee@naver.com>
2019-12-07 02:35:22 +0900

    <수정>백준 5052번 전화번호 목록 주석 수정

--- baekjoon/baekjoon5052.py
+++ baekjoon/baekjoon5052.py
@@ -5,14 +5,13 @@ testcase=int(input())#테스트케이스
 
 
 def solution(result):
-
-    flag=result[0]
+    flag=result[0] #비교 대상
 
     for i in result[1:]:
-        if flag in i[0:len(flag)]:
+        if flag in i[0:len(flag)]:#처음부터 flag값의 길이까지 비교
             return "NO"
         else:
-            flag=i
+            flag=i#flag 값 교체
 
     return "YES"
 
@@ -27,7 +26,7 @@ for i in range(testcase):
         result.append(phonenum)#길이, 번호 순으로 result에 저장
     result.sort()#길이순서로 정렬
 
-    ans.append(solution(result))
+    ans.append(solution(result))#solution 결과를 ans에 푸시!
 
 
 for i in ans:
