2653ed31f43046828ce3a43b854e7c699340df6d
ksshlee <ksshlee@naver.com>
2019-12-31 16:46:33 +0900

    <수정> python dfs 알고리즘 코드 추가

--- graph/graph.py
+++ graph/graph.py
@@ -9,12 +9,12 @@ def bfs(graph,start):
     while q.qsize()>0:
         node = q.get()
         # 만약 현재 노드의 방문 기록이 없다면 추가
-        if node not in visit:
+        if visit.get(node) == None:
             visit[node]=True
         
         # 해당 노드의 인접 노드 큐에 추가
         for x in graph[node]:
-            if x not in visit:
+            if visit.get(node) == None:
                 q.put(x)            
         
     ans=[x for x in visit]
@@ -22,17 +22,39 @@ def bfs(graph,start):
     return ans
 
 
+def dfs(graph,start):
+    visit = {}
+    s = [start]
+
+
+    #s가 존재할때
+    while s:
+        node = s.pop()
+        if visit.get(node) == None:
+            visit[node]=True
+        
+        for x in graph[node]:
+            if visit.get(x) == None:
+                s.append(x)
+        
+
+    ans = [x for x in visit]
+
+    return ans
+
 
     
 
 
 
 
-graph={0 : [1,2,3,4] , 
-1 : [0,2,4], 
-2 : [0,1,3], 
-3 : [0,2], 
-4 : [0,1]
+graph={'A' : ['B','C','E'] , 
+'B' : ['D','F'], 
+'C' : ['G'], 
+'D' : [],
+'E' : ['F'], 
+'F' : ['E'],
+'G' : []
 }
 
-print(bfs(graph,0))
+print(dfs(graph,'A'))
